f5ecc7508dc07ea0d41d2bc195f5206044d911e6
Ilya Sergeev <ilya.srgw@gmail.com>
2024-04-28 17:19:15 +0300

    Initial commit

---
+++ src/components/Timer.js
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+
+function Timer({ dispatch, secondsRemaining }) {
+  const mins = Math.floor(secondsRemaining / 60);
+  const seconds = secondsRemaining % 60;
+
+  useEffect(
+    function () {
+      const id = setInterval(() => {
+        dispatch({ type: "tick" });
+      }, 1000);
+
+      return function () {
+        clearInterval(id);
+      }; // runs between renders and after component has unmounted
+    },
+    [dispatch]
+  );
+
+  return (
+    <div className="timer">
+      {mins < 10 && "0"}
+      {mins}:{seconds < 10 && "0"}
+      {seconds}
+    </div>
+  );
+}
+
+export default Timer;
